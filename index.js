311eeb78403d0c6034ab6bd9d06dbfb0ad0d1424
Tasneem Nabi <Tasneem@lawn-128-61-113-102.lawn.gatech.edu>
2016-11-15 16:09:31 -0500

    x

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.post('/webhook/', function (req, res) {
               sendGenericImage(sender, result[0].imageUrl, 'http://version1.api.memegenerator.net/Generators_Select_ByPopular?pageSize=1&days=1')
             }
           })
+        )
       } else if (text.indexOf('link') > -1) {
         console.log('link')
         // Memify using existing link
