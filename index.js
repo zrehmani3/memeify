a7766539001e3fe946eac7505f6d732a9f6cc3aa
Zain Rehmani <zrehmani@fb.com>
2016-12-14 02:20:59 -0800

    x

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ expressApp.post('/webhook/', function (req, res) {
           sendGenericErrorMessage(sender);
         }
       }
-    } else if (event.message.attachments) {
+    } else if (event.message && event.message.attachments) {
       // We are uploading an image only
       let download = function(uri, filename, callback) {
         request.head(uri, function(err, res, body) {
