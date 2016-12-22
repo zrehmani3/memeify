58f5367c78c694a33ec960ba90519ed6e32f7362
Zain Rehmani <zrehmani@fb.com>
2016-12-22 14:58:00 -0800

    x

--- index.js
+++ index.js
@@ -797,3 +797,44 @@ function sendHelpMessage(sender) {
     }
   })
 }
+
+/**
+  Persistent menu CTA Script:
+  curl -X POST -H "Content-Type: application/json" -d '{
+    "setting_type" : "call_to_actions",
+    "thread_state" : "existing_thread",
+    "call_to_actions":[
+      {
+        "type":"postback",
+        "title":"#help",
+        "payload":"HELP"
+      },
+      {
+        "type":"postback",
+        "title":"#advanced",
+        "payload":"ADVANCED"
+      },
+      {
+        "type":"postback",
+        "title":"#search for memes",
+        "payload":"SEARCH"
+      },
+      {
+        "type":"postback",
+        "title":"#upload your own image",
+        "payload":"UPLOAD"
+      },
+    ]
+  }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=process.env.TOKEN"
+
+  Get started CTA Script:
+  curl -X POST -H "Content-Type: application/json" -d '{
+    "setting_type":"call_to_actions",
+    "thread_state":"new_thread",
+    "call_to_actions":[
+      {
+        "payload":"GET_STARTED"
+      }
+    ]
+  }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=process.env.TOKEN"
+*/
