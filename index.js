924292cd912c5dd98a0c4c5682257104631ebdf3
Zain Rehmani <zrehmani@fb.com>
2017-02-13 21:42:18 -0800

    x

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ expressApp.listen(expressApp.get('port'), function() {
 expressApp.post('/webhook/', function (req, res) {
   let messaging_events = req.body.entry[0].messaging
   for (let i = 0; i < messaging_events.length; i++) {
-    console.log('hi');
+    console.log('hweri');
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
     if (event.message && event.message.text) {
